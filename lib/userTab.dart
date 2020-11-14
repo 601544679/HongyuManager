fe19e1a12c4bf2defc585736e8d0e350a873172b
601544679 <601544679@qq.com>
2020-11-14 15:14:28 +0800

    ios

--- lib/userTab.dart
+++ lib/userTab.dart
@@ -94,7 +94,7 @@ class _userMessageState extends State<userMessage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
+      child: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(
@@ -111,68 +111,67 @@ class _userMessageState extends State<userMessage> {
             SizedBox(
               height: ScreenUtil().setHeight(70),
             ),
-            Expanded(
-              flex: 1,
-              child: ListView.separated(
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.only(
-                        left: ScreenUtil().setWidth(75),
-                        right: ScreenUtil().setWidth(75)),
-                    child: Material(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(15),
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            left: ScreenUtil().setWidth(37.5),
-                            right: ScreenUtil().setWidth(37.5),
-                            top: ScreenUtil().setHeight(26),
-                            bottom: ScreenUtil().setHeight(26)),
-                        child: Row(
-                          children: [
-                            Text(
-                              '${titleList[index]}：${widget.contentList[index]}',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: ScreenUtil()
-                                      .setSp(30, allowFontScalingSelf: true)),
-                            )
-                          ],
-                        ),
+            ListView.separated(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(
+                      left: ScreenUtil().setWidth(75),
+                      right: ScreenUtil().setWidth(75)),
+                  child: Material(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(15),
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: ScreenUtil().setWidth(37.5),
+                          right: ScreenUtil().setWidth(37.5),
+                          top: ScreenUtil().setHeight(26),
+                          bottom: ScreenUtil().setHeight(26)),
+                      child: Row(
+                        children: [
+                          Text(
+                            '${titleList[index]}：${widget.contentList[index]}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: ScreenUtil()
+                                    .setSp(30, allowFontScalingSelf: true)),
+                          )
+                        ],
                       ),
                     ),
-                  );
-                },
-                itemCount: titleList.length,
-                separatorBuilder: (context, index) {
-                  return SizedBox(
-                    height: ScreenUtil().setHeight(40),
-                  );
-                },
+                  ),
+                );
+              },
+              itemCount: titleList.length,
+              separatorBuilder: (context, index) {
+                return SizedBox(
+                  height: ScreenUtil().setHeight(40),
+                );
+              },
+            ),
+            SizedBox(
+              height: ScreenUtil().setHeight(35),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: Material(
+                color: Colors.indigo[colorNum],
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      left: ScreenUtil().setWidth(30),
+                      right: ScreenUtil().setWidth(30)),
+                  child: Text(
+                    '退出登录',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: ScreenUtil().setSp(40),
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                borderRadius: BorderRadius.circular(15),
               ),
             ),
-            Expanded(
-              child: MaterialButton(
-                onPressed: () {},
-                child: Material(
-                  color: Colors.indigo[colorNum],
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                        left: ScreenUtil().setWidth(30),
-                        right: ScreenUtil().setWidth(30)),
-                    child: Text(
-                      '退出登录',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: ScreenUtil().setSp(40),
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-              ),
-              flex: 1,
-            )
           ],
         ),
       ),
